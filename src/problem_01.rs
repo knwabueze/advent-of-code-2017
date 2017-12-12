1d1e40089bdaa1ef18e2f1d531c31fcd441eff80
Kosi Nwabueze <kosinwabueze@gmail.com>
2017-12-12 13:57:24 -0800

    Added star 2 of problem 1

--- src/problem_01.rs
+++ src/problem_01.rs
@@ -10,7 +10,6 @@ fn inverse_captcha(sequence: &str) -> u32 {
             .skip(1)
             .chain(sequence.chars().take(1)),
     ) {
-        println!("a: {}, b: {}", a, b);
         if a == b {
             match a.to_digit(10) {
                 Some(n) => acc += n,
@@ -22,12 +21,32 @@ fn inverse_captcha(sequence: &str) -> u32 {
     acc
 }
 
+fn inverse_captcha_2(sequence: &str) -> u32 {
+    let mut acc: u32 = 0;
+    let len = sequence.len();
+
+    for (a, b) in sequence.chars().enumerate().filter(|a| a.0 < len / 2)
+        .zip(sequence.chars().enumerate().filter(|a| a.0 >= len / 2))
+        {
+            if a.1 == b.1 {
+                acc += match b.1.to_digit(10) {
+                    Some(n) => n * 2,
+                    None => 0
+                };
+            }
+        }
+
+    acc
+}
+
+
 fn main() {
     println!("{}", inverse_captcha(INPUT));
+    println!("{}", inverse_captcha_2(INPUT));
 }
 
 #[cfg(test)]
-mod test_suite {
+mod test_suite_1 {
     use super::inverse_captcha;
 
     #[test]
@@ -48,5 +67,35 @@ mod test_suite {
     #[test]
     fn sequence_results_in_nine() {
         assert_eq!(inverse_captcha("91212129"), 9);
+    }
+}
+
+#[cfg(test)]
+mod test_suite_2 {
+    use super::inverse_captcha_2;
+
+    #[test]
+    fn sequence_results_in_6() {
+        assert_eq!(inverse_captcha_2("1212"), 6);
+    }   
+
+    #[test]
+    fn sequence_results_in_0() {
+        assert_eq!(inverse_captcha_2("1221"), 0);
+    } 
+
+    #[test]
+    fn sequence_results_in_4() {
+        assert_eq!(inverse_captcha_2("123425"), 4);
+    }
+
+    #[test]
+    fn sequence_results_in_12() {
+        assert_eq!(inverse_captcha_2("123123"), 12);
+    }
+
+    #[test]
+    fn sequence_results_in_4_2() {
+        assert_eq!(inverse_captcha_2("12131415"), 4);
     }
 }
